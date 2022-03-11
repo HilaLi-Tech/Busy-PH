7b39af52c1388c5078b6d4f2cb9e5ef6155ee271
Thea Foronda <tdcforonda@gmail.com>
2022-03-11 13:18:24 +0800

    initiated shop page

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -25,12 +25,90 @@ class _ShopState extends State<Shop> {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return Scaffold(
       appBar: const PreferredSize(
         preferredSize: const Size.fromHeight(150.0),
         child: AppBarBusy(),
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Header
+           Stack(
+             overflow: Overflow.visible,
+                    children: [
+                      Row(children:[
+                      Expanded(
+                  child: Container(
+                      margin: EdgeInsets.all(10),
+                      // height: 100,
+                      height: 100,
+                      decoration:  BoxDecoration(
+                          // borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/Top_Phone_Repair.png'),
+                            fit: BoxFit.fitWidth,
+                            alignment: FractionalOffset.topCenter,
+                          )
+                      )
+                  )
+                )]),
+                      Positioned(
+                          top: 90,
+                          left: 20,
+                          child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration:  BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              border: Border.all(color: Colors.white, width: 10),
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/Corporate_Dudebros.jpg'),
+                                fit: BoxFit.cover,
+                                alignment: FractionalOffset.topCenter,
+                              )
+                          )
+                      )),
+                      Positioned(
+                        left: 130,
+                          top: 130,
+                          child: Text("Marky's Phone Repair",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w700),)),
+                      Positioned(
+                          left: 130,
+                          top: 155,
+                          child: Container(
+                            height:20,
+                            width: 60,
+                            child: Text("Follow",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.blue, fontSize: 12)),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue, width: 2)
+                            ),
+                          ))
+                    ]),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40, 80, 20, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                "Open Hours: 10 AM - 5 PM",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+              Text(
+                "Location: Quezon City (500m from Home)",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+          ]))])
+      ),
       bottomNavigationBar: const BottomNaviBar(),
     );
   }
